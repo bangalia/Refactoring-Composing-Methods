6a451797dde8d65a301e3b80bb379ef78ad63d5f
Anita Bangali <anitabangali@yahoo.com>
2022-03-04 13:11:36 -0600

    Completed exercises
    Files were moved from the larger testing and architecture repository

---
+++ burger_toppings.py
@@ -0,0 +1,18 @@
+# by Kami Bigdely
+# Split temporary variable
+
+patty_weight = 70 # [gr]
+pickle_weight = 20 # [gr]
+tomatoes_weight = 25 # [gr]
+lettuce_weight = 15 # [gr]
+buns_weight = 95 # [gr]
+ny_burger = (2 * patty_weight + 4 * pickle_weight + 3 * tomatoes_weight + 2 * lettuce_weight
+                + 2 * buns_weight)
+print("NY Burger Weight:", ny_burger, "grams")
+kimchi_weight = 30 # [gr]
+mayo_weight = 5 # [gr]
+golden_fried_onion_weight = 20 # [gr]
+seoul_burger = (2 * patty_weight + 4 * pickle_weight + 3 * tomatoes_weight
+                + kimchi_weight + mayo_weight + golden_fried_onion_weight + 2 * buns_weight)
+print("Seoul Kimchi Burger Weight:", seoul_burger,"grams")
+
---
+++ kinetic_energy.py
@@ -0,0 +1,38 @@
+# By Kami Bigdely
+# Remove assignment to method parameter.
+
+class Distance:
+    def __init__(self, d_value, d_unit):
+        self.d_unit = d_unit
+        self.d_value = d_value
+
+class Mass:
+    def __init__(self, value, unit):
+        self.value = value
+        self.unit = unit
+
+def calculate_kinetic_energy(mass, distance, time):
+    if distance.d_unit != 'km' and distance.d_unit == "ly":
+        # if distance.unit == "ly":  # [ly] stands for light-year (measure of distance in astronomy)
+            # convert light-year to km unit
+        in_km = distance.d_value * 9.461e12
+        converted_distance = Distance(in_km, "km")
+    else:
+        print ("unit is Unknown")
+        return
+    speed = converted_distance.d_value/time # [km per sec]
+    if mass.unit != 'kg':
+        if mass.unit == "solar-mass":
+            # convert from solar mass to kg
+            value = mass.value * 1.98892e30 # [kg]
+            converted_mass = Mass(value, 'kg')
+        else:
+            print ("unit is Unknown")
+            return
+
+    kinetic_energy = 0.5 * converted_mass.value * speed ** 2
+    return kinetic_energy
+
+mass = Mass(2, "solar-mass")
+distance = Distance(2, 'ly')
+print(calculate_kinetic_energy(mass, distance, 3600e20))
